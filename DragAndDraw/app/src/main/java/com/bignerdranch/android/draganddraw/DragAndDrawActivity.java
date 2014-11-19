b67f45710940f768ff8e86db8b0ef631533a2693
hdantas <hndantas@gmail.com>
2014-11-19 15:56:21 +0000

    Copy-pasted code from Cyril Mottier to implement fading action bar on scroll.

--- DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/DragAndDrawActivity.java
@@ -1,12 +1,58 @@
 package com.bignerdranch.android.draganddraw;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.KeyEvent;
+import android.widget.ScrollView;
 
 
 public class DragAndDrawActivity extends SingleFragmentActivity {
 
     DragAndDrawFragment mDragAndDrawFragment;
+    private Drawable mActionBarBackgroundDrawable;
+
+    private NotifyingScrollView.OnScrollChangedListener mOnScrollChangedListener = new NotifyingScrollView.OnScrollChangedListener() {
+        public void onScrollChanged(ScrollView who, int l, int t, int oldl, int oldt) {
+            final int headerHeight = findViewById(R.id.image_header).getHeight() - getSupportActionBar().getHeight();
+            final float ratio = (float) Math.min(Math.max(t, 0), headerHeight) / headerHeight;
+            final int newAlpha = (int) (ratio * 255);
+            mActionBarBackgroundDrawable.setAlpha(newAlpha);
+        }
+    };
+
+    private Drawable.Callback mDrawableCallback = new Drawable.Callback() {
+        @Override
+        public void invalidateDrawable(Drawable who) {
+            getSupportActionBar().setBackgroundDrawable(who);
+        }
+
+        @Override
+        public void scheduleDrawable(Drawable who, Runnable what, long when) {
+        }
+
+        @Override
+        public void unscheduleDrawable(Drawable who, Runnable what) {
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mActionBarBackgroundDrawable = getResources().getDrawable(R.color.red);
+        mActionBarBackgroundDrawable.setAlpha(0);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            mActionBarBackgroundDrawable.setCallback(mDrawableCallback);
+        }
+
+        getSupportActionBar().setBackgroundDrawable(mActionBarBackgroundDrawable);
+
+        ((NotifyingScrollView) findViewById(R.id.scroll_view)).setOnScrollChangedListener(mOnScrollChangedListener);
+
+    }
 
     @Override
     protected Fragment createFragment() {
