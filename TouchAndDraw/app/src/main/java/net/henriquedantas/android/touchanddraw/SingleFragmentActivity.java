da15e3337e7821f5ab1893fca9f743361eda051c
hdantas <hndantas@gmail.com>
2014-12-06 16:04:14 +0000

    Forced showing overflow menu in all devices.

--- TouchAndDraw/app/src/main/java/net/henriquedantas/android/touchanddraw/SingleFragmentActivity.java
+++ TouchAndDraw/app/src/main/java/net/henriquedantas/android/touchanddraw/SingleFragmentActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Field;
 
 /**
  * Created by hdantas on 10/8/14.
@@ -31,6 +35,25 @@ public abstract class SingleFragmentActivity extends ActionBarActivity {
         if (fragment == null) {
             fragment = createFragment();
             fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
+
+        showOverflowMenu();
+    }
+
+    /**
+     * Hack to show overflow menu in ALL devices
+     * src: https://stackoverflow.com/questions/15492791/how-do-i-show-overflow-menu-items-to-action-bar-in-android
+     **/
+    private void showOverflowMenu() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if (menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "showOverflowMenu", e);
         }
     }
 }
